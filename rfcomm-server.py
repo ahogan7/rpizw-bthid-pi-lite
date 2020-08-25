ed1a0dd0b1b7646c29a61f1da1cf483b98aa4a11
Names <names@PC-M7AE5G.lan>
2020-08-25 12:14:18 -0400

    hid i/o bug squash

--- rfcomm-server.py
+++ rfcomm-server.py
@@ -72,6 +72,7 @@ class AESCipher:
     def _unpad(s):
         return s[:-ord(s[len(s) - 1:])]
 
+# characters
 
 NUC = chr(0)
 SHF = chr(32)+NUC
@@ -81,12 +82,12 @@ DN = NUC*5
 FRE = NUC*8
 map = [0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]
 regular = ["", "", "", "", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1",
-                       "2", "3", "4", "5", "6", "7", "8", "9", "0", "ENTER", "ESCAPE", "\b", "\t", " ", "-", "=", "[", "]", "\\", "#", ";", "'", "`", ",", ".", "/",
+                       "2", "3", "4", "5", "6", "7", "8", "9", "0", "ENTER", "ESCAPE", "\b", "\t", " ", "-", "=", "[", "]", "\\", "", ";", "'", "`", ",", ".", "/",
                        "CAPSLOCK", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "printscreen", "Scroll Lock", "Pause",
                        "Insert", "Home", "PageUp", "Delete", "End", "PageDown", "RightArrow", "LeftArrow", "DownArrow", "UpArrow",
                        "Num Lock ", "/1", "*", "-", "+", "ENTER", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ""]
 shifted = ["", "", "", "", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-                      "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "", "", "", "", "", "_", "+", "{", "}", "|", "~", ":", "\"", "~", "<", ">", "?", "", "", "", "", "", "", "",
+                      "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "", "", "", "", "", "_", "+", "{", "}", "|", "", ":", "\"", "~", "<", ">", "?", "", "", "", "", "", "", "",
                       "", "", "", "", "", "", "insert", "", "", "", "", "", "Forward", "", "", "", "", "", "", "Clear", "", "", "", "", "", "End", "Down Arrow", "PageDn",
                       "Left Arrow", "", "Rigth Arrow", "Home", "Up Arrow", "PageUp", "Insert", "Delete", ""]
 num = """0123456789"""
@@ -221,8 +222,8 @@ if __name__ == '__main__':
     advertise_service( server_sock, "SampleServer",
                    service_id = uuid,
                    service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ],
-#                   protocols = [ OBEX_UUID ]
+                   profiles = [ SERIAL_PORT_PROFILE ], 
+#                   protocols = [ OBEX_UUID ] 
                     )
 
     client_sock, client_info = server_sock.accept()
